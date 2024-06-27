28ff8ee7589a072611966aba7a5c17cd50ce603a
oneonlee <dg.lee@postech.ac.kr>
2024-06-27 05:50:55 +0000

    fix json to jsonl

--- scripts/ft_dataset_labeling.sh
+++ scripts/ft_dataset_labeling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python classifier/ft_dataset/labeling/query_labeling.py \
-    --large_model_eval_result_path classifier/ft_dataset/labeling/auto_evaluation/result/meta-llama-3-70b-instruct_evaluated_by_gpt-3.5-turbo-0125.json \
-    --small_model_eval_result_path classifier/ft_dataset/labeling/auto_evaluation/result/meta-llama-3-8b-instruct_evaluated_by_gpt-3.5-turbo-0125.json \
+    --large_model_eval_result_path classifier/ft_dataset/labeling/auto_evaluation/result/meta-llama-3-70b-instruct_evaluated_by_gpt-3.5-turbo-0125.jsonl \
+    --small_model_eval_result_path classifier/ft_dataset/labeling/auto_evaluation/result/meta-llama-3-8b-instruct_evaluated_by_gpt-3.5-turbo-0125.jsonl \
     --output_path classifier/ft_dataset/labeling/auto_evaluation/result
 
