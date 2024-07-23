6ece0b7d2d64ebee2dcfccf7675adbac92bf7e44
oneonlee <dg.lee@postech.ac.kr>
2024-07-23 10:04:06 +0000

    update for gpt-4o-mini

--- scripts/CRAG_evaluate_for_labeling.sh
+++ scripts/CRAG_evaluate_for_labeling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-evaluator_model="gpt-4o-2024-05-13" #`gpt-4o-2024-05-13`, `gpt-4-0125-preview`, or `gpt-3.5-turbo-0125`
+evaluator_model="gpt-4o-2024-05-13" #`gpt-4o-2024-05-13` or `gpt-4o-mini-2024-07-18`
 
 for target_model_name in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Meta-Llama-3-70B-Instruct"; do
     python classifier/ft_dataset/labeling/auto_evaluation/RAG_evaluation_auto.py \
