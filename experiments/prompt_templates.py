68d40f584f3c6cd75ebf8306509105fa806e08ff
oneonlee <dg.lee@postech.ac.kr>
2024-07-24 12:15:24 +0000

    update RAG_SYS_PROMPT & RAG_PROMPT_TEMPLATE

--- experiments/prompt_templates.py
+++ experiments/prompt_templates.py
@@ -1,10 +1,9 @@
 #################### Prompts for RAG ####################
 RAG_SYS_PROMPT = """You are an assistant for answering questions.
 You are given the extracted parts of a long document and a question.
-Refer to the passage below and answer the following question shortly and concisely.
-If you don't know the answer, just say "I do not know." Don't make up an answer."""
+Refer to the passage below and answer the following question shortly and concisely."""
 
-RAG_PROMPT_TEMPLATE = "Passages: {passages} \n\n Question: {query} \n\n Answer: "
+RAG_PROMPT_TEMPLATE = "Instruction: {instruction}\n\nPassages: {passages}\n\nQuestion: {query}\n\nAnswer: "
 RAG_PROMPT_TEMPLATE_SUFFIX = "Answer: "
 
 
