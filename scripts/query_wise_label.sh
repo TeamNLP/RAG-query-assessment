427121746e14d9db976921f866b3615d2fdc756e
oneonlee <dg.lee@postech.ac.kr>
2024-08-09 07:13:48 +0000

    update

--- scripts/query_wise_label.sh
+++ scripts/query_wise_label.sh
@@ -3,5 +3,6 @@
 python classifier/ft_dataset/labeling/query_wise_label.py \
     --seed 13370 \
     --eval_result_path classifier/ft_dataset/evaluation/result \
-    --performance_threshold 0.5 \
+    --large_performance_threshold 0.5 \
+    --small_performance_threshold 0.5 \
     --result_dataset_path classifier/ft_dataset
